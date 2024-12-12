601ce5467e9551fa4294f614dcc052d5b60ca21d
fbseong <109562549+fbseong@users.noreply.github.com>
2024-12-12 17:03:39 +0900

    first commit

--- lib/feature/home_screen.dart
+++ lib/feature/home_screen.dart
@@ -1,1 +1,17 @@
-// class HomeScreen extends Consumer
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeScreen extends ConsumerStatefulWidget {
+  @override
+  ConsumerState<HomeScreen> createState() => _HomeScreen();
+
+}
+
+class _HomeScreen extends ConsumerState<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.black);
+  }
+
+}
---
+++ lib/util/GlobalColor.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class GlobalColor {
+  static const Color primaryColor = Color(0xFF00438A);
+  static const Color indexBoxColor = Color(0xFFF3F5F7);
+}
