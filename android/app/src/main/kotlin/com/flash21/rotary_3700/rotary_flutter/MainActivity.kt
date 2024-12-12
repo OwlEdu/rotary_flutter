4327646d54770cd03edec043a69f85336ac2556f
fbseong <109562549+fbseong@users.noreply.github.com>
2024-12-12 16:40:20 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/flash21/rotary_3700/rotary_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flash21.rotary_3700.rotary_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/feature/home/home_main_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeMainScreen  extends StatelessWidget{
+  const HomeMainScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    throw Container();
+  }
+}
