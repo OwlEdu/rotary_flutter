39652d44eb9f880df9ab7455e56c05c6edfbc408
김민균 <101560577+mingyun0809@users.noreply.github.com>
2024-12-16 13:59:50 +0900

    공지사항 페이지 수정

--- lib/feature/home/menu/notice_screen.dart
+++ lib/feature/home/menu/notice_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:rotary_flutter/util/global_color.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class NoticeScreen extends StatefulWidget {
@@ -21,16 +20,26 @@ class _NoticeScreenState extends State<NoticeScreen> {
       ..loadRequest(Uri.parse('http://rotary3700.or.kr/bsbbs/list?page=2&tn=notice&ti=&sw=&st=&cate=0'));
   }
 
+  Future<bool> _onWillPop() async {
+    if(await _controller.canGoBack()) {
+      await _controller.goBack();
+      return false;
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      backgroundColor: GlobalColor.white,
-      appBar: AppBar(
-        title: Text('공지사항'),
-        centerTitle: true,
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        backgroundColor: GlobalColor.white,
+        appBar: AppBar(
+          title: Text('공지사항'),
+          centerTitle: true,
+        ),
+        body: WebViewWidget(controller: _controller),
       ),
-      body: WebViewWidget(controller: _controller),
     );
   }
 }
