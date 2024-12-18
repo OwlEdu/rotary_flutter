b675bc68c3368f8d8f12b0a9af8f12edc6ab47e7
fbseong <109562549+fbseong@users.noreply.github.com>
2024-12-18 14:48:30 +0900

    repository

--- lib/feature/myInfo/my_info_screen.dart
+++ lib/feature/myInfo/my_info_screen.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:go_router/go_router.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:rotary_flutter/data/account/account_remote_data.dart';
+import 'package:rotary_flutter/util/fontSize.dart';
 import 'package:rotary_flutter/util/secure_storage.dart';
 
+import '../../data/model/account_model.dart';
 import '../../util/common.dart';
 
-class MyInfoScreen extends StatelessWidget {
+class MyInfoScreen extends ConsumerStatefulWidget {
   const MyInfoScreen({super.key});
+
+  ConsumerState<MyInfoScreen> createState() => _MyInfoScreen();
+}
+class _MyInfoScreen extends ConsumerState<MyInfoScreen>{
+  Account? myData;
+
+  void getMyData() async{
+    var temp = await AccountAPI().getAccount(cellphone: '010-5829-2280');
+
+   setState(() {
+    myData = temp?.first;
+   });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    getMyData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,25 +43,26 @@ class MyInfoScreen extends StatelessWidget {
           // Image.network('$BASE_URL/qr?size=10&data=tel:+01058292280'),
           Container(width: 200, height: 200, color: Colors.red),
 
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.all(15),
-            child: Column(children: [
-              Text('회원정보'),
-              SvgPicture.asset(
-                width: 25,
-                height: 25,
-                'asset/icons/router/user_search_outline_icon.svg',
-              ),
-              Spacer(),
-              Text('수정하기')
-            ],)
-          ),
+          // Container(
+          //   width: double.infinity,
+          //   padding: EdgeInsets.all(15),
+          //   child: Column(children: [
+          //     Text('회원정보'),
+          //     SvgPicture.asset(
+          //       width: 25,
+          //       height: 25,
+          //       'asset/icons/router/user_search_outline_icon.svg',
+          //     ),
+          //     Spacer(),
+          //     Text('수정하기')
+          //   ],)
+          // ),
 
           InkWell(
-              child: Text('hello'),
+              child: Text('${myData?.name??''}', style: TextStyle(fontSize: DynamicFontSize.font35(context)),),
               onTap: () {
-                context.push('/menu/userInfo');
+
+                // context.push('/menu/userInfo');
               }),
         ],
       ),
